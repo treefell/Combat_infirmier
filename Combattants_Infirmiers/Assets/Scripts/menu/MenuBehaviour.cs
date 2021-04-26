a066a91ea91815d4dfaa03555772cc844e1fe4ac
ironman <charlesli.huang@gmail.com>
2021-04-26 18:43:31 +0200

    script for menu updated not working with Xbox controller yet

--- Combattants_Infirmiers/Assets/Scripts/menu/MenuBehaviour.cs
+++ Combattants_Infirmiers/Assets/Scripts/menu/MenuBehaviour.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 public class MenuBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
+    int i;
+    Button[] buttons;
+    
     void Start()
     {
-        
+        int i = 0;
+        buttons = this.GetComponentsInChildren<Button>(true);
+        buttons[i].Select();
     }
  
     public void Start_Game()
@@ -29,6 +36,9 @@ public class MenuBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        buttons[i].Select();
+        i = (i +((int)Input.GetAxis("Vertical"))) % 2;
+
+
     }
 }
